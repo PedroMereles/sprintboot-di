2530007dacea4f4365159062279337ba41670ca0
Pedro Mereles <pedromereles83@gmail.com>
2024-11-03 21:04:57 -0300

    testes

---
+++ src/main/java/com/pedrom/sprintboot/di/app/sprintbootdi/controllers/SomeController.java
@@ -0,0 +1,33 @@
+package com.pedrom.sprintboot.di.app.sprintbootdi.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrom.sprintboot.di.app.sprintbootdi.models.Product;
+import com.pedrom.sprintboot.di.app.sprintbootdi.services.ProductService;
+
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService service = new ProductService();
+    @GetMapping
+    public List<Product> list(){
+        return service.findAll();
+
+    }
+
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return service.findById(id);
+
+    }
+    
+
+}
--- src/main/java/com/pedrom/sprintboot/di/app/sprintbootdi/services/ProductService.java
+++ src/main/java/com/pedrom/sprintboot/di/app/sprintbootdi/services/ProductService.java
@@ -1,7 +1,26 @@
 package com.pedrom.sprintboot.di.app.sprintbootdi.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.pedrom.sprintboot.di.app.sprintbootdi.models.Product;
+import com.pedrom.sprintboot.di.app.sprintbootdi.repositories.ProductRepository;
+
 public class ProductService {
+    /**
+     * @return
+     */
+    private ProductRepository repository = new ProductRepository();
 
-    
+   public List<Product> findAll(){
+    return repository.finadAll().stream().map(p ->{
+        Double priceImp = p.getPrice() * 1.25d;
+        p.setPrice(priceImp.longValue());
+        return p;
+    }).collect(Collectors.toList());
+   } 
 
+   public Product findById(Long id){
+        return repository.findById(id);
+   }
 }
